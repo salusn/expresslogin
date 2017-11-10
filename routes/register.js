c5ed6fa1f6b1bb2338270ae48fbceba6b6965c7c
salusn <salu.s@lightrains.com>
2017-11-10 17:55:39 +0530

    Working on session

--- routes/register.js
+++ routes/register.js
@@ -5,6 +5,7 @@ var session = require('express-session')
 var validator = require('express-validator');
 router.use(validator())
 var MongoClient = require('mongodb').MongoClient;
+assert = require('assert');
 var url = 'mongodb://localhost:27017/formdb';
 
 router.get('/register', function(req, res, next) {
@@ -21,21 +22,20 @@ router.post('/register', function(req, res) {
 		username = req.body.username;
 		password = req.body.password;
 		cpassword = req.body.cpassword;
-		
-		res.redirect('welcome');
-        
-	// MongoClient.connect(url, function(err, db) {
-	//   if (err) return
-	// assert.equal(null, err);
-	//   var collection = db.collection('formdata')
-	//   collection.insert({firstname: firstname, lastname: lastname}, function(err, result) {
-
-	//      if(result){
-	//      	res.redirect('success')
-	 
-	//      }
-	//   })
-	// })
+		        
+	MongoClient.connect(url, function(err, db) {
+	  if (err) return
+	assert.equal(null, err);
+	  var collection = db.collection('formaction')
+	  collection.insert({firstname: firstname, lastname: lastname, email: email, number: number, username: username, password: password}, function(err, result) {
+//console.log(result);
+	     if(result){
+	     	//req.session.firstname = firstname;
+	     	console.log(req)
+	     	res.redirect('welcome')	 
+	     }
+	  })
+	})
 });
 
 
